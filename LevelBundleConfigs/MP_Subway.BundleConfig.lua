53a8a0dbe67883c078f0977da97ef06c936bbad3
Dumpy250 <cameron250taylor@outlook.com>
2023-06-19 17:28:07 -0400

    chore(metro): added metro bundle

---
+++ LevelBundleConfigs/MP_Subway.BundleConfig.lua
@@ -0,0 +1,21 @@
+return {
+	{
+		gamemodes = {
+			"AdvanceAndSecureStd",
+			"AdvanceAndSecureAlt",
+			"ConquestAssaultLarge0",
+			"SkirmishStd",
+			"TrainingStd"
+		},
+		config = {
+			terrainAssetName = 'Levels/MP_Subway/Terrain/MP_Subway_Terrain',
+			superBundles = {
+				'MpChunks',
+				'Levels/MP_Subway/MP_Subway',
+			},
+			bundles = {
+				'Levels/MP_Subway/MP_Subway',
+			}
+		}
+	}
+}
