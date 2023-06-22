f074ee1b90ef504b1208f716bd2e289f55ed0b4f
Dumpy250 <cameron250taylor@outlook.com>
2023-06-22 14:37:05 -0400

    tweak(bundleloader): tweaked metro bc

--- LevelBundleConfigs/MP_Subway.BundleConfig.lua
+++ LevelBundleConfigs/MP_Subway.BundleConfig.lua
@@ -5,16 +5,22 @@ return {
 			"AdvanceAndSecureAlt",
 			"ConquestAssaultLarge0",
 			"SkirmishStd",
-			"TrainingStd"
+			"TrainingStd",
+			"SquadDeathMatch0"
 		},
 		config = {
 			terrainAssetName = 'Levels/MP_Subway/Terrain/MP_Subway_Terrain',
 			superBundles = {
-				'MpChunks',
+				'levels/coop_003/coop_003',
 				'Levels/MP_Subway/MP_Subway',
 			},
 			bundles = {
-				'Levels/MP_Subway/MP_Subway',
+			'levels/mp_subway/mp_subway',
+            'levels/mp_subway/squaddeathmatch',
+            'levels/coop_003/coop_003',
+            'levels/coop_003/ab02_art_parent',
+            'levels/coop_003/ab00_art_parent',
+            'levels/coop_003/ab00_art_sewer_parent',
 			}
 		}
 	}
