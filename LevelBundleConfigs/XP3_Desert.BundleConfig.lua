e97ca25b0fdd00719b21f2f49f703e03f41bbff0
Dumpy250 <cameron250taylor@outlook.com>
2023-08-26 22:52:29 -0400

    feat(main): added bandar bundle config

---
+++ LevelBundleConfigs/XP3_Desert.BundleConfig.lua
@@ -0,0 +1,20 @@
+return {
+	{
+		gamemodes = {
+			"AdvanceAndSecureStd",
+			"AdvanceAndSecureAlt",
+			"ConquestLarge0",
+			"SkirmishStd",
+			"TrainingStd"
+		},
+		config = {
+			terrainAssetName = 'Levels/XP3_Desert/Terrain/Terrain_8k',
+			superBundles = {
+				'levels/MP_017/MP_017',
+			},
+			bundles = {
+				'levels/MP_017/MP_017',
+			},
+		}
+	}
+}
