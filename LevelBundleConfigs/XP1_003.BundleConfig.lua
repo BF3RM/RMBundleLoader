114fc7f62d237920326ddee5b7477df7f49cdaa7
Dumpy <cameron250taylor@outlook.com>
2023-11-02 20:47:04 -0400

    chore(main): modified Sharqi bundle config

--- LevelBundleConfigs/XP1_003.BundleConfig.lua
+++ LevelBundleConfigs/XP1_003.BundleConfig.lua
@@ -9,6 +9,21 @@ return {
 		},
 		config = {
             terrainAssetName = 'Levels/XP1_003/XPACK1_003_Terrain', --XPACK1_001_Terrain.txt/json
+			superBundles = {
+				'Levels/Coop_010/Coop_010',
+				'Levels/MP_013/MP_013'
+			},
+			bundles = {
+				'Levels/Coop_010/Coop_010',
+				'Levels/MP_013/MP_013' -- For vodnik
+			},
+			dynamicBundles = {
+				'Levels/MP_013/ConquestLarge'
+			},
+			dynamicRegistries = {
+				-- 'MP_013_ConquestLarge_Registry'
+				DC(Guid('1CEC6C7E-1629-4631-B326-1A134BC6EF27'), Guid('6196137B-50D6-4607-98AE-900BACF47065'))
+			}
         }
 	}
 }
