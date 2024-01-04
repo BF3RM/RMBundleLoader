b6ad5a17d610f1b3a2c0ddbf516199bf0bbb4d37
MotorcycleEnjoyer <116019878+MotorcycleEnjoyer@users.noreply.github.com>
2024-01-04 01:24:28 -0500

    feat(main): add COOP_009 bundle to sharqi for insurgency mode

--- LevelBundleConfigs/XP1_003.BundleConfig.lua
+++ LevelBundleConfigs/XP1_003.BundleConfig.lua
@@ -12,14 +12,17 @@ return {
             terrainAssetName = 'Levels/XP1_003/XPACK1_003_Terrain', --XPACK1_001_Terrain.txt/json
 			superBundles = {
 				'Levels/Coop_010/Coop_010',
-				'Levels/MP_013/MP_013'
+				'Levels/MP_013/MP_013',
+				'Levels/COOP_009/AB00_Parent'
 			},
 			bundles = {
 				'Levels/Coop_010/Coop_010',
-				'Levels/MP_013/MP_013' -- For vodnik
+				'Levels/MP_013/MP_013', -- For vodnik
+				'Levels/COOP_009/AB00_Parent'
 			},
 			dynamicBundles = {
-				'Levels/MP_013/ConquestLarge'
+				'Levels/MP_013/ConquestLarge',
+				'Levels/COOP_009/AB00_Parent'
 			},
 			dynamicRegistries = {
 				-- 'MP_013_ConquestLarge_Registry'
