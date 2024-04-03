b8c54fd851a2021f74fa0f79fc0f39d16f168a6c
Dumpy <cameron250taylor@outlook.com>
2024-04-03 02:07:18 -0400

    refactor(bundleloader): add oman

---
+++ BundleConfig/Levels/XP1_002.lua
@@ -0,0 +1,29 @@
+return {
+	terrainAssetName = 'Levels/XP1_002/Terrain_2/GulfTerrain_03', --XPACK1_001_Terrain.txt/json
+	superBundles = {
+        'Xp5Chunks',
+        'Levels/XP5_001/XP5_001',
+	},
+	bundles = {
+		[ResourceCompartment.ResourceCompartment_Game] = {
+            'Levels/XP5_001/XP5_001',
+            'Levels/XP5_001/Air_Superiority',
+		},
+		[4] = {
+			'Levels/MP_013/ConquestLarge'
+		}
+	},
+	registries = {
+		[4] = {
+
+        }
+	},
+	uiBundles = {
+	},
+	blueprintGuidsToBlock = {
+
+	},
+	exceptionGameModeList = {
+
+	}
+}
