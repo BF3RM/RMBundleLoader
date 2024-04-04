d22c7facf80f1e86ce989aa3a3f2caba90621724
Ori <buhbutoriasher@gmail.com>
2024-04-04 03:25:45 +0300

    add all maps

---
+++ BundleConfig/Levels/XP2_Palace.lua
@@ -0,0 +1,17 @@
+return {
+	terrainAssetName = nil, --XPACK1_001_Terrain.txt/json
+	superBundles = {
+	},
+	bundles = {
+		[ResourceCompartment.ResourceCompartment_Game] = {
+		},
+	},
+	registries = {
+	},
+	uiBundles = {
+	},
+	blueprintGuidsToBlock = {
+	},
+	exceptionGameModeList = {
+	}
+}
---
+++ BundleConfig/Levels/XP4_Rubble.lua
@@ -0,0 +1,17 @@
+return {
+	terrainAssetName = 'Levels/XP4_Rubble/Terrain/Terrain',
+	superBundles = {
+	},
+	bundles = {
+		[ResourceCompartment.ResourceCompartment_Game] = {
+		},
+	},
+	registries = {
+	},
+	uiBundles = {
+	},
+	blueprintGuidsToBlock = {
+	},
+	exceptionGameModeList = {
+	}
+}
