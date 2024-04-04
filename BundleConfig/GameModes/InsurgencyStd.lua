82eed4963f7de4d98be6d0ca01e9753e4fce0ba2
Ori <buhbutoriasher@gmail.com>
2024-04-05 01:45:31 +0300

    add SP_Earthquake to INS mode

--- BundleConfig/GameModes/InsurgencyStd.lua
+++ BundleConfig/GameModes/InsurgencyStd.lua
@@ -1,12 +1,18 @@
 return {
+	superBundles = {
+		'Levels/SP_Earthquake/SP_Earthquake'
+	},
 	bundles = {
 		[ResourceCompartment.ResourceCompartment_Game] = {
-			"Levels/COOP_009/AB00_Parent"
+			"Levels/COOP_009/AB00_Parent",
+			'Levels/SP_Earthquake/SP_Earthquake',
+			"Levels/SP_Earthquake/StreetCrossing"
 		}
 	},
 	registries = {
 		[ResourceCompartment.ResourceCompartment_Game] = {
-			"Levels/COOP_009/AB00_Parent"
+			"Levels/COOP_009/AB00_Parent",
+			"Levels/SP_Earthquake/StreetCrossing"
 		}
 	},
 	uiBundles = {
