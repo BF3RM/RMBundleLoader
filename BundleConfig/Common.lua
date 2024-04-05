741a1edebdec9bef9d86a6bdba24601475b48a18
Ori <buhbutoriasher@gmail.com>
2024-04-05 03:51:49 +0300

    come on

--- BundleConfig/Common.lua
+++ BundleConfig/Common.lua
@@ -4,7 +4,7 @@ return {
 		'Levels/SP_Tank/SP_Tank',
 		'Levels/SP_Bank/SP_Bank',
 		'Levels/Coop_006/Coop_006',
-		'Levels/Coop_009/Coop_009',
+		'Levels/Coop_009/Coop_009'
 	},
 	bundles = {
 		[ResourceCompartment.ResourceCompartment_Game] = {
@@ -13,7 +13,7 @@ return {
 			"Levels/SP_Bank/SP_Bank",
 			"Levels/SP_Bank/Ride_SUB",
 			'Levels/Coop_006/Coop_006',
-			'Levels/Coop_009/Coop_009',
+			'Levels/Coop_009/Coop_009'
 		}
 	},
 	registries = {
