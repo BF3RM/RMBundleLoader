af8756f198ebdb143a2726821152ce5448cbb14c
Ori <buhbutoriasher@gmail.com>
2024-05-25 18:07:25 +0300

    add Vodnik to Azadi

--- BundleConfig/Levels/XP4_Parl.lua
+++ BundleConfig/Levels/XP4_Parl.lua
@@ -2,17 +2,20 @@ return {
 	terrainAssetName = 'Levels/XP4_Parliament/Terrain_Parliament/Terrain_Parliament',
 	superBundles = {
 		'Levels/Coop_010/Coop_010',
-		'Levels/FrontEnd/FrontEnd',
+		'Levels/MP_013/MP_013',
 	},
 	bundles = {
 		[ResourceCompartment.ResourceCompartment_Game] = {
 			'Levels/Coop_010/Coop_010',
-			'Levels/FrontEnd/FrontEnd', --For mp characters, effects and all weapons
+			'Levels/MP_013/MP_013' -- For vodnik
+		},
+		[4] = {
+			'Levels/MP_013/ConquestLarge'
 		}
 	},
 	registries = {
 		[4] = {
-			'Levels/FrontEnd/FrontEnd'
+			'Levels/MP_013/ConquestLarge'
 		}
 	},
 	uiBundles = {
