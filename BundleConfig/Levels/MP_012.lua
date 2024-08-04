909751cecbfb41c4d42913a0f4478e908e03fb19
Ori <buhbutoriasher@gmail.com>
2024-08-04 10:39:38 +0300

    add Firestorm first commit

---
+++ BundleConfig/Levels/MP_012.lua
@@ -0,0 +1,27 @@
+return {
+	terrainAssetName = 'Levels/MP_012/Terrain/MP_012', 
+	superBundles = {
+		'Levels/MP_013/MP_013',
+	},
+	bundles = {
+		[ResourceCompartment.ResourceCompartment_Game] = {
+			'Levels/MP_013/MP_013'
+		},
+		[4] = {
+			'Levels/MP_013/ConquestLarge'
+		}
+	},
+	registries = {
+		[4] = {
+			'Levels/MP_013/ConquestLarge'
+		}
+	},
+	uiBundles = {
+	},
+	blueprintGuidsToBlock = {
+
+	},
+	exceptionGameModeList = {
+
+	}
+}
