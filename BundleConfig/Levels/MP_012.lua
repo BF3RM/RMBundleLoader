f3f8df1a256d0e6dfbb5749c7fe7f16124ab62c7
Ori <buhbutoriasher@gmail.com>
2024-08-04 11:10:08 +0300

    update MP_012

--- BundleConfig/Levels/MP_012.lua
+++ BundleConfig/Levels/MP_012.lua
@@ -1,19 +1,19 @@
 return {
 	terrainAssetName = 'Levels/MP_012/Terrain/MP_012', 
 	superBundles = {
-		'Levels/MP_013/MP_013',
+		'Levels/XP1_002/XP1_002',
 	},
 	bundles = {
 		[ResourceCompartment.ResourceCompartment_Game] = {
-			'Levels/MP_013/MP_013'
+			'Levels/XP1_002/XP1_002',
 		},
 		[4] = {
-			'Levels/MP_013/ConquestLarge'
+			'Levels/XP1_002/CQ_L',
 		}
 	},
 	registries = {
 		[4] = {
-			'Levels/MP_013/ConquestLarge'
+			'Levels/XP1_002/CQ_L',
 		}
 	},
 	uiBundles = {
