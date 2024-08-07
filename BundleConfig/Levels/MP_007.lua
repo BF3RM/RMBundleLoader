2afd189e127fb36c539f6389644aca87826d67e5
Ori <buhbutoriasher@gmail.com>
2024-08-07 07:18:46 +0300

    add Caspian Border

---
+++ BundleConfig/Levels/MP_007.lua
@@ -0,0 +1,17 @@
+return {
+	terrainAssetName = 'Levels/MP_007/Terrain/MP_007',
+	superBundles = {
+	},
+	bundles = {
+	},
+	registries = {
+	},
+	uiBundles = {
+	},
+	blueprintGuidsToBlock = {
+
+	},
+	exceptionGameModeList = {
+
+	}
+}
