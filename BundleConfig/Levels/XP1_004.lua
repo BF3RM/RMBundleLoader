e89b27872d90ff85857c7692d6523c6e55ec51e0
Ori <buhbutoriasher@gmail.com>
2024-09-04 12:47:19 +0300

    add wake island

---
+++ BundleConfig/Levels/XP1_004.lua
@@ -0,0 +1,15 @@
+return {
+	terrainAssetName = 'Levels/XP1_004/XP1_004_Terrain8k',
+	superBundles = {
+	},
+	bundles = {
+	},
+	registries = {
+	},
+	uiBundles = {
+	},
+	blueprintGuidsToBlock = {
+	},
+	exceptionGameModeList = {
+	}
+}
