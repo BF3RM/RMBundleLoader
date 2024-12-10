b2ec543645191bd63aa36d3d4abdf83ca6efc3d0
Ori <buhbutoriasher@gmail.com>
2024-12-10 14:07:24 +0200

    add MP_013

---
+++ BundleConfig/Levels/MP_013.lua
@@ -0,0 +1,20 @@
+return {
+	terrainAssetName = 'levels/mp_013/terrain/mp013_testterrain', 
+	superBundles = {
+
+	},
+	bundles = {
+
+	},
+	registries = {
+
+	},
+	uiBundles = {
+	},
+	blueprintGuidsToBlock = {
+
+	},
+	exceptionGameModeList = {
+
+	}
+}
