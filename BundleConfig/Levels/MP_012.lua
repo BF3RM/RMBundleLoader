c5ba3bfefa44fb599e19261edecee8364868cd6b
Ori <buhbutoriasher@gmail.com>
2025-05-31 00:26:10 +0300

    remove bundles from MP_012

--- BundleConfig/Levels/MP_012.lua
+++ BundleConfig/Levels/MP_012.lua
@@ -1,20 +1,10 @@
 return {
 	terrainAssetName = 'Levels/MP_012/Terrain/MP_012', 
 	superBundles = {
-		'Levels/XP1_002/XP1_002',
 	},
 	bundles = {
-		[ResourceCompartment.ResourceCompartment_Game] = {
-			'Levels/XP1_002/XP1_002',
-		},
-		[4] = {
-			'Levels/XP1_002/CQ_L',
-		}
 	},
 	registries = {
-		[4] = {
-			'Levels/XP1_002/CQ_L',
-		}
 	},
 	uiBundles = {
 	},
