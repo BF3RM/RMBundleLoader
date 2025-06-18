0d45666dcad9c6d9c652aa8ee4badf2a0ec3f679
Ori <buhbutoriasher@gmail.com>
2025-06-18 20:28:03 +0300

    add MP_013 to Wake Island

--- BundleConfig/Levels/XP1_004.lua
+++ BundleConfig/Levels/XP1_004.lua
@@ -1,19 +1,22 @@
 return {
 	terrainAssetName = 'Levels/XP1_004/XP1_004_Terrain8k',
 	superBundles = {
-		'Levels/XP1_001/XP1_001'
+		'Levels/MP_013/MP_013'
 	},
 	bundles = {
-		[ResourceCompartment.ResourceCompartment_Game] = {
-			'Levels/XP1_001/XP1_001'
+		[3] = {
+			'Levels/MP_013/MP_013'
 		},
 		[4] = {
-			'Levels/XP1_001/Conquest_Large'
+			'levels/MP_013/ConquestLarge'
 		}
 	},
 	registries = {
+		[3] = {
+			'Levels/MP_013/MP_013'
+		},
 		[4] = {
-			'Levels/XP1_001/Conquest_Large'
+			'levels/MP_013/ConquestLarge'
 		}
 	},
 	uiBundles = {
