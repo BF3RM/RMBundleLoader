73a19cdfbc8a3eacf2da270f4198f5de61035448
Ori <buhbutoriasher@gmail.com>
2025-06-26 00:27:34 +0300

    - add MP_012 to Markaz
    - fix XP4_Rubble (exclude MP_001)

--- BundleConfig/Levels/XP4_FD.lua
+++ BundleConfig/Levels/XP4_FD.lua
@@ -1,17 +1,26 @@
 return {
-	terrainAssetName = 'Levels/XP4_FinancialDistrict/Terrain4k/XP4_FD_Terrain/XP4_FD_Terrain',
-	superBundles = {
-	},
-	bundles = {
-		[ResourceCompartment.ResourceCompartment_Game] = {
-		},
-	},
-	registries = {
-	},
-	uiBundles = {
-	},
-	blueprintGuidsToBlock = {
-	},
-	exceptionGameModeList = {
-	}
+    terrainAssetName = 'Levels/XP4_FinancialDistrict/Terrain4k/XP4_FD_Terrain/XP4_FD_Terrain',
+    superBundles = {
+        'MpChunks',
+        'Levels/MP_012/MP_012',
+    },
+    bundles = {
+        [ResourceCompartment.ResourceCompartment_Game] = {
+            'Levels/MP_012/MP_012'
+        },
+		[4] = {
+			'levels/mp_012/tutorialmp'
+        }
+    },
+    registries = {
+		[4] = {
+			'levels/mp_012/tutorialmp'
+        }
+    },
+    uiBundles = {
+    },
+    blueprintGuidsToBlock = {
+    },
+    exceptionGameModeList = {
+    }
 }
