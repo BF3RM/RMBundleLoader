4479e8a43d313868c98af8318080c40fcbcd2500
Ori <buhbutoriasher@gmail.com>
2025-06-27 21:35:32 +0300

    add korner and tow2 for valley

--- BundleConfig/Levels/XP3_Valley.lua
+++ BundleConfig/Levels/XP3_Valley.lua
@@ -1,12 +1,20 @@
 return {
 	terrainAssetName = 'Levels/MP_Canyon/MP_Canyon_Terrain/MP_Canyon_Terrain/MP_Canyon_Terrain',
 	superBundles = {
-
+		'levels/XP5_002/XP5_002'
 	},
 	bundles = {
-
+		[3] = {
+			'levels/XP5_002/XP5_002'
+		},
+		[4] = {
+			'levels/XP5_002/ctf'
+		}
 	},
 	registries = {
+		[4] = {
+			'levels/XP5_002/ctf'
+		}
 	},
 	uiBundles = {
 	},
