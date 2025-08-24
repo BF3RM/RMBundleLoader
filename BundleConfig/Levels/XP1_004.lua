77d03f834fb16cbf3f90e6032d8dab2f66f8fc01
Ori <buhbutoriasher@gmail.com>
2025-08-24 20:13:07 +0300

    Add KA60 to Wake Island

--- BundleConfig/Levels/XP1_004.lua
+++ BundleConfig/Levels/XP1_004.lua
@@ -1,22 +1,28 @@
 return {
 	terrainAssetName = 'Levels/XP1_004/XP1_004_Terrain8k',
 	superBundles = {
+		'MpChunks',
+		'Levels/MP_012/MP_012',
 		'Levels/MP_013/MP_013'
 	},
 	bundles = {
 		[3] = {
-			'Levels/MP_013/MP_013'
+			'Levels/MP_013/MP_013',
+			'Levels/MP_012/MP_012'
 		},
 		[4] = {
-			'levels/MP_013/ConquestLarge'
+			'levels/MP_013/ConquestLarge',
+			'Levels/MP_012/Conquest_Large'
 		}
 	},
 	registries = {
 		[3] = {
-			'Levels/MP_013/MP_013'
+			'Levels/MP_013/MP_013',
+			'Levels/MP_012/MP_012'
 		},
 		[4] = {
-			'levels/MP_013/ConquestLarge'
+			'levels/MP_013/ConquestLarge',
+			'Levels/MP_012/Conquest_Large'
 		}
 	},
 	uiBundles = {
