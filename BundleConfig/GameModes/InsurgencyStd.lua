312d3dfb38e715aacce01f21e6ef324ec3aed101
Ori <buhbutoriasher@gmail.com>
2026-02-07 10:47:00 +0200

    remove unused bundle for INS game mode

--- BundleConfig/GameModes/InsurgencyStd.lua
+++ BundleConfig/GameModes/InsurgencyStd.lua
@@ -1,13 +1,11 @@
 return {
 	bundles = {
 		[ResourceCompartment.ResourceCompartment_Game] = {
-			"Levels/COOP_009/AB00_Parent"
 		}
 
 	},
 	registries = {
 		[ResourceCompartment.ResourceCompartment_Game] = {
-			"Levels/COOP_009/AB00_Parent"
 		}
 
 	},
